6b84c7c9e84d6882a3fe5767abb7dc83a4ae88ea
jeong kwangyoung <holyanegl@gmail.com>
2024-10-17 07:15:18 +0900

    Spring Security Filter,Config

---
+++ src/main/java/com/nkidol/board_back/filter/JwtAuthenticationFilter.java
@@ -0,0 +1,84 @@
+package com.nkidol.board_back.filter;
+
+import java.io.IOException;
+
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.nkidol.board_back.provider.JwtProvider;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    
+    private final JwtProvider jwtProvider;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest req, HttpServletResponse res, FilterChain filterChain)
+            throws ServletException, IOException {
+
+
+            try {
+                String token = parseBearerToken(req);
+
+            if (token == null) {
+                filterChain.doFilter(req, res);
+                return;
+            }
+
+            String email = jwtProvider.validate(token);
+
+            if (email == null) {
+                filterChain.doFilter(req, res);
+                return;
+            }
+
+            AbstractAuthenticationToken authenticationToken = 
+                new UsernamePasswordAuthenticationToken(email, null,AuthorityUtils.NO_AUTHORITIES);
+
+            authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(req));
+
+            SecurityContext securityContext = SecurityContextHolder.createEmptyContext();
+            securityContext.setAuthentication(authenticationToken);
+
+            SecurityContextHolder.setContext(securityContext);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            filterChain.doFilter(req, res);
+
+            
+
+            
+    }
+
+    private String parseBearerToken(HttpServletRequest req){
+
+        String authorization = req.getHeader("Authorization");
+
+        boolean hasAuthorization = StringUtils.hasText(authorization);
+        if (!hasAuthorization) return null;
+
+        boolean isBearer = authorization.startsWith("Bearer ");
+        if (!isBearer) return null;
+
+        String token = authorization.substring(7);
+
+        return token;
+
+    }
+}
--- src/main/java/com/nkidol/board_back/provider/JwtProvider.java
+++ src/main/java/com/nkidol/board_back/provider/JwtProvider.java
@@ -6,6 +6,7 @@ import java.time.Instant;
 import java.time.temporal.ChronoUnit;
 import java.util.Date;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import io.jsonwebtoken.Claims;
@@ -16,7 +17,8 @@ import io.jsonwebtoken.security.Keys;
 @Component
 public class JwtProvider {
     
-    private String secretKey = "S3cr3tK3y";
+    @Value("${secret-key}")
+    private String secretKey;
 
     public String create(String email) {
 
